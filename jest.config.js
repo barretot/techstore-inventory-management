fa8748b9fbadd8f98c1b9bfe93faad811b72e6cf
oBarreto <barretoruan77@gmail.com>
2024-07-09 03:23:52 -0300

    ci test

--- jest.config.js
+++ jest.config.js
@@ -3,26 +3,7 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: '<rootDir>/coverage/unit',
   coverageProvider: 'v8',
-  coverageReporters: ['text', 'html'],
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100
-    }
-  },
-  collectCoverageFrom: [
-    'src/v1/domains/**/*.js'
-  ],
-  roots: ['test/v1/unit']
-}
-module.exports = {
-  watchPathIgnorePatterns: ['globalConfig'],
-  collectCoverage: true,
-  coverageDirectory: '<rootDir>/coverage/unit',
-  coverageProvider: 'v8',
-  coverageReporters: ['text', 'html'],
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
   coverageThreshold: {
     global: {
       branches: 100,
